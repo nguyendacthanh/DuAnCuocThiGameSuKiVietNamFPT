2d0b51ec1f59fd6049ae9738864cf0e912e681df
nguyendacthanh <thanhndph23033@fpt.edu.vn>
2025-08-08 22:18:43 +0700

    v5.5 chỉnh sửa để AI áp dụng với đối tượng mới là City

--- Assets/script/REMAKE/AIAction.cs
+++ Assets/script/REMAKE/AIAction.cs
@@ -1,17 +1,19 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AIAction
 {
+    
     public static GameObject FindNearestArmy(GameObject enemy)
     {
         GameObject[] listUnitPlayer = GameObject.FindGameObjectsWithTag("Player");
-        Vector3 viTriEnemy = enemy.transform.position;
+        Vector3 enemyPos = enemy.transform.position;
         float minDistance = float.MaxValue;
         GameObject nearestTargetUnit = null;
         foreach (GameObject unitPlayer in listUnitPlayer)
         {
-            float distance = Vector3.Distance(viTriEnemy, unitPlayer.transform.position);
+            float distance = Vector3.Distance(enemyPos, unitPlayer.transform.position);
             if (distance < minDistance)
             {
                 minDistance = distance;
@@ -20,35 +22,88 @@ public class AIAction
         }
         return nearestTargetUnit;
     }
+    public static GameObject FindNearestCity(GameObject enemy)
+    {
+        GameObject[] listTargetCity = GameObject.FindGameObjectsWithTag("City");
+        Vector3 enemyPos = enemy.transform.position;
+        float minDistance = float.MaxValue;
+        GameObject nearestTarget = null;
+        foreach (GameObject targetCity in listTargetCity)
+        {
+            if (targetCity.GetComponent<ClassCity>().isPlayerCity)
+            {
+                float distance = Vector3.Distance(enemyPos, targetCity.transform.position);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    nearestTarget = targetCity;
+                }
+            }
+        }
+        return nearestTarget;
+    }
+    public static bool isNearestTargetIsCity(GameObject enemy)
+    {
+        GameObject[] listPlayerUnit = GameObject.FindGameObjectsWithTag("Player");
+        GameObject[] listCities = GameObject.FindGameObjectsWithTag("City");
+        Vector3 enemyPos = enemy.transform.position;
+        float nearestPlayerDist = float.MaxValue;
+        foreach (var player in listPlayerUnit)
+            nearestPlayerDist = Mathf.Min(nearestPlayerDist, Vector3.Distance(enemyPos, player.transform.position));
+        float nearestCityDist = float.MaxValue;
+        foreach (var city in listCities)
+            if(city.GetComponent<ClassCity>().isPlayerCity)
+                nearestCityDist = Mathf.Min(nearestCityDist, Vector3.Distance(enemyPos, city.transform.position));
+        return nearestCityDist <= nearestPlayerDist;
+    }
 
-    public static bool IsPlayerInRange(GameObject enemy, GameObject player)
+    public static bool IsTargetInRange(GameObject enemy, GameObject target)
     {
         ClassUnit enemyUnit = enemy.GetComponent<ClassUnit>();
         Vector3 enemyPos = enemy.transform.position;
-        Vector3 playerPos = player.transform.position;
+        Vector3 targetPos = target.transform.position;
 
         float range = enemyUnit.RangeAtk * 100f;
-        float distance = Mathf.Abs(enemyPos.x - playerPos.x) + Mathf.Abs(enemyPos.y - playerPos.y);
+        float distance = Mathf.Abs(enemyPos.x - targetPos.x) + Mathf.Abs(enemyPos.y - targetPos.y);
 
-        return distance <= range;
+        return distance < range;
     }
 
     public static void EnemyMove(GameObject enemy)
     {
+        bool isTargetIsCity;
         ClassUnit enemyUnit = enemy.GetComponent<ClassUnit>();
-        GameObject nearestPlayer = FindNearestArmy(enemy);
-
-        if (nearestPlayer == null)
-            return;
-
-        if (IsPlayerInRange(enemy, nearestPlayer))
+        GameObject nearestTarget;
+        if (isNearestTargetIsCity(enemy))
         {
-            enemyUnit.Attack(nearestPlayer);
-            enemyUnit.CurrentSpeed = Mathf.Max(0, enemyUnit.CurrentSpeed - 1);
+            nearestTarget = FindNearestCity(enemy);
+            isTargetIsCity = true;
+        }
+        else
+        {
+            nearestTarget = FindNearestArmy(enemy);
+            isTargetIsCity = false;
+        }
+        if (nearestTarget == null)
             return;
+        Vector3 moveToPos;
+        if (IsTargetInRange(enemy, nearestTarget))
+        {
+            if (isTargetIsCity && nearestTarget.GetComponent<ClassCity>().cityHp>0)
+            {
+                nearestTarget.GetComponent<ClassCity>().TakeDamage(enemyUnit.TotalDame(),nearestTarget);
+                enemyUnit.CurrentSpeed = Mathf.Max(0, enemyUnit.CurrentSpeed - 1);
+                return;
+            }
+            else if (!isTargetIsCity)
+            {
+                enemyUnit.Attack(nearestTarget);
+                enemyUnit.CurrentSpeed = Mathf.Max(0, enemyUnit.CurrentSpeed - 1);
+                return;
+            }
         }
 
-        Vector3 moveToPos = FindBestMovePosition(enemy, nearestPlayer);
+        moveToPos = FindBestMovePosition(enemy, nearestTarget);
 
         // Nếu tìm được vị trí để di chuyển
         if (moveToPos != enemy.transform.position)
@@ -57,9 +112,9 @@ public class AIAction
         }
 
         // Sau khi di chuyển, nếu đã vào tầm thì tấn công
-        if (IsPlayerInRange(enemy, nearestPlayer))
+        if (IsTargetInRange(enemy, nearestTarget))
         {
-            enemyUnit.Attack(nearestPlayer);
+            enemyUnit.Attack(nearestTarget);
             enemyUnit.CurrentSpeed = Mathf.Max(0, enemyUnit.CurrentSpeed - 1);
         }
     }
