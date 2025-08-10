b3c5edf65c9ca1a39547cc2b438a3a6501fe0d62
nguyendacthanh <thanhndph23033@fpt.edu.vn>
2025-08-10 10:39:00 +0700

    new sprite

--- Assets/script/UI/SYSTEM.cs
+++ Assets/script/UI/SYSTEM.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -5,10 +7,27 @@ public class SYSTEM : MonoBehaviour
 {
     // public TextMeshProUGUI text;
     public GameObject gameObj;
+    private Animator animator;
+
+    [Header("Âm thanh")]
+    public AudioSource audioSource;
+    public AudioClip soundOn;
+
+    private float time = 1f; // thời gian chạy âm thanh
+
+    private void Start()
+    {
+        animator = gameObj.GetComponent<Animator>();
+
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+    }
 
     public void SetActiveTrue()
     {
+        animator.SetTrigger("a");
         gameObj.SetActive(true);
+        StartCoroutine(PlaySoundForOneSecond());
     }
 
     public void SetActiveFalse()
@@ -16,9 +35,15 @@ public class SYSTEM : MonoBehaviour
         gameObj.SetActive(false);
     }
 
-    public void other()
+    private IEnumerator PlaySoundForOneSecond()
     {
-        
+        if (soundOn != null && audioSource != null)
+        {
+            audioSource.clip = soundOn;
+            audioSource.Play();
+            yield return new WaitForSeconds(time);
+            audioSource.Stop();
+        }
     }
 
 }
